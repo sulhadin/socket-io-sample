14e19ad246dde390ee3b58a879fe36dbbd77baa9
sulhadin <sulhadin@gmail.com>
2021-02-14 22:17:43 +0300

    feat: Added socket client to project.

--- server/index.js
+++ server/index.js
@@ -1,18 +1,24 @@
-const server = require('http').createServer();
-const io = require('socket.io')(server);
+const app = require('express')();
+const http = require('http').createServer(app);
+const io = require('socket.io')(http,{
+    cors: {
+      origin: "http://localhost:3000",
+      methods: ["GET", "POST"]
+    }
+  });
 
-io.on('connection', (client) => {
-    client.on('message', (data) => {
+io.on('connection', (socket) => {
+    socket.on('message', (data) => {
         console.log('data', data);
         
         io.emit('message', data);
     });
-    client.on('disconnect', () => { 
+    
+    socket.on('disconnect', () => { 
         console.log('disconnected');
      });
-
 });
 
-server.listen(4000, () => {
+http.listen(4000, () => {
     console.log('listenning.'); 
 });
--- web/src/App.js
+++ web/src/App.js
@@ -1,22 +1,35 @@
-import logo from './logo.svg';
+import { useEffect, useState} from 'react'
 import './App.css';
 
+import io from "socket.io-client"
+
+const socket = io.connect('http://localhost:4000')
+
 function App() {
+  const [state, setState] = useState([]);
+  const [value, setValue] = useState('');
+  const onSubmit = () => {
+    socket.emit('message', value);
+  }
+
+  useEffect(() => {
+    socket.on('message', (data) => {
+      setState(prev=>{
+        const test = [...prev, data];
+        
+        return test;
+      });
+    });
+  }, [])
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        Chat APP
+        {state.map(d=><p key={d}>{d}</p>)}
+        {value}
+      <input value={value} onChange={e=>setValue(e.target.value)}></input>
+      <button onClick={onSubmit}>Submit</button>
       </header>
     </div>
   );
